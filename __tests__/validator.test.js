6d595b8421035c0ead89c502397909764b00d27d
adrianhuebner <adrianh92@gmail.com>
2019-10-14 19:42:08 -0700

    more code added to tests

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -40,15 +40,15 @@ describe('validator module performs basic validation of', () => {
 
 describe('validator module performs complex validations', () => {
 
-  // it('validates the presence of required object properties at any level', () => {
-  //   // i.e. does person.hair.color exist and have a good value, not just person.hair
-  //   expect(true).toBeFalsy();
-  // });
+  it('validates the presence of required object properties at any level', () => {
+    // i.e. does person.hair.color exist and have a good value, not just person.hair
+    
+  });
 
-  // it('validates the proper types of object properties', () => {
-  //   // i.e. person.name must be a string, etc.
-  //   expect(true).toBeFalsy();
-  // });
+  it('validates the proper types of object properties', () => {
+    // i.e. person.name must be a string, etc.
+    expect(true).toBeFalsy();
+  });
 
   it('validates the types of values contained in an array', () => {
     let arr = ['a', 'b', 'c', 'd'];
@@ -62,10 +62,10 @@ describe('validator module performs complex validations', () => {
     expect(validator.typeArray(badArr)).toBeFalsy();
   });
 
-  // it('validates a value array against an approved list', () => {
-  //   // i.e. a string might only be allowed to be "yes" or "no"
-  //   expect(true).toBeFalsy();
-  // });
+  it('validates a value array against an approved list', () => {
+    // i.e. a string might only be allowed to be "yes" or "no"
+    expect(true).toBeFalsy();
+  });
 
   // TODO: Cover so, so many more cases
 
